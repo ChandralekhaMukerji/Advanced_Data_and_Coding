41067c6f27c940c783ebea1755c279098a4ebfc4
ChandralekhaMukerji <ChandralekhaMukerji@github.com>
2018-03-25 13:55:48 +0100

    adding data files

---
+++ datafiles/Data frame cleaning.R
@@ -0,0 +1,92 @@
+# load packages#
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(gridExtra)
+library(openxlsx)
+
+#Cleaning Regional Danish Population Vs Immigrants#
+Danish_population <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/Danish_populationQ17.csv", 
+                              col_names = FALSE)
+Danish_population.clean <- Danish_population %>% 
+  select(-X1, -X2)
+names(Danish_population.clean) <- c("region", "total", "Danes")
+#calculations of % of immigrant population by region#
+Danish_population.clean<- Danish_population.clean %>%
+  mutate(immigrants = total-Danes, ImmigrantPercentage= immigrants/total*100)
+
+#Cleaning of economic activity rate data#
+economic_activity <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/economic_activity.csv", 
+    col_names = FALSE)
+economic_activity.clean <- economic_activity %>% 
+  select(-X1)
+names(economic_activity.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
+
+#Economic Activity rate mean#
+economic_activity.clean.melt <- melt(economic_activity.clean, id="region")
+names(economic_activity.clean.melt) <- c("region", "year", "immigrants")
+economic_activity.clean.melt$year <- as.numeric(as.character(economic_activity.clean.melt$year))
+economic_activity.clean1 <- economic_activity.clean.melt
+economic_activity.mean.clean1.mean <- aggregate(economic_activity.clean1[, 3], list(economic_activity.clean1$region), mean)
+names(economic_activity.mean.clean1.mean) <- c("region", "Economic Activity Rate")
+
+#Cleaning unemployment benefits#
+Unemployment_benefits <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/Unemployment_benefits.csv", 
+                                  col_names = FALSE)
+Unemployment_benefits.clean <- Unemployment_benefits %>% 
+  select(-X1)
+names(Unemployment_benefits.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
+
+#Unemployment benefits mean for last 10 years#
+Unemployment_benefits.clean.melt <- melt(Unemployment_benefits.clean, id="region")
+names(Unemployment_benefits.clean.melt) <- c("region", "year", "benefits")
+Unemployment_benefits.clean.melt$year <- as.numeric(as.character(Unemployment_benefits.clean.melt$year))
+Unemployment_benefits.clean1 <- Unemployment_benefits.clean.melt
+Unemployment_benefits.clean1.mean <- aggregate(Unemployment_benefits.clean1[, 3], list(Unemployment_benefits.clean1$region), mean)
+names(Unemployment_benefits.clean1.mean) <- c("region", "Avg_Benefits")
+
+# merge the two datasets#
+Danish_population.merge <- merge(Danish_population.clean, Unemployment_benefits.clean1.mean, by="region")
+Danish_population.merge1 <- merge(Danish_population.merge,economic_activity.mean.clean1.mean, by="region")
+
+
+#Cleaning of employment rate data#
+employment_rate <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/employment_rate.csv", 
+                                                         col_names = FALSE)
+employment_rate.clean <- employment_rate %>% 
+  select(-X1)
+names(employment_rate.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
+
+
+#Cleaning of unemployment data#
+unemployement <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/unemployement.csv", 
+                          col_names = FALSE)
+unemployement.clean <- unemployement %>% 
+  select(-X1)
+names(unemployement.clean) <- c("region", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
+
+#Unemployement mean#
+unemployement.clean.melt <- melt(unemployement.clean, id="region")
+names(unemployement.clean.melt) <- c("region", "year", "immigrants")
+unemployement.clean.melt$year <- as.numeric(as.character(unemployement.clean.melt$year))
+unemployement.clean1 <- unemployement.clean.melt
+unemployement.clean1.mean <- aggregate(unemployement.clean1[, 3], list(unemployement.clean1$region), mean)
+names(unemployement.clean1.mean) <- c("region", "Avg. Unemployment Rate")
+
+# merge the two datasets#
+Danish_population.merge2 <- merge(Danish_population.merge1,unemployement.clean1.mean, by="region")
+
+# calculating percentage of population claiming unemployment benefits#
+Danish_population.FINAL<- Danish_population.merge2 %>%
+  mutate(percentage_claiming_benefits = Avg_Benefits/total*100)
+
+#cleaning table keeping only relevant columns#
+Danish_population.FINAL1 <- Danish_population.FINAL %>% 
+  select(-Danes, -immigrants, -Avg_Benefits)
+
+#scatter plot made using plotly#
+
+
+
---
+++ datafiles/WorldImmigration.R
@@ -0,0 +1,57 @@
+#install xlsx package#
+install.packages("openxlsx", dependencies=TRUE)
+
+#loading packages#
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(gridExtra)
+library(openxlsx)
+
+
+#Number of people who have immigrated to Denmark in last 30 years#
+world_immigration <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/world_immigration.csv", 
+                              col_names = FALSE)
+world_immigration.clean <- world_immigration %>% 
+  select(-X1, -X3, -X4, -X5, -X6, -X7, -X8, -X9)
+names(world_immigration.clean) <- c("region","1987", "1988","1989","1990","1991", "1992", "1993", "1994", "1995", "1996","1997", "1998","1999","2000","2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009","2010","2011","2012", "2013", "2014", "2015", "2016", "2017")
+
+#melting the data#
+world_immigration.clean.melt <- melt(world_immigration.clean, id="region")
+names(world_immigration.clean.melt) <- c("region", "year", "immigrants")
+world_immigration.clean.melt$year <- as.numeric(as.character(world_immigration.clean.melt$year))
+world_immigration.clean1 <- world_immigration.clean.melt
+
+#sum all 30 years for each country#
+world_immigration_TOTAL <- world_immigration.clean1 %>%
+  mutate(immigrants = as.numeric(as.character(immigrants))) %>%  
+  group_by(region) %>%
+  summarise(count = sum(immigrants, na.rm = FALSE ))
+
+#converting to excel for dropdown data#
+write.xlsx(world_immigration_TOTAL, file = "C:/Users/Me/Documents/interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/world_immigration_TOTAL.xlsx", asTable = TRUE)
+
+#cleaning for matching with world bank population data#
+world_immigration_TOTAL_Clean <- world_immigration_TOTAL[-c(1, 3, 12, 30, 47, 54, 58, 59, 67, 68, 74, 73, 75, 78, 81, 85, 96, 131, 135, 147, 152, 154, 155, 157, 159, 170, 174, 175, 176, 177, 184, 187, 194, 197, 198, 200, 202, 209, 216, 229, 233, 235, 236), ]
+
+
+#world population cleaning, data from world bank#
+world_populationWB <- read_csv("interactive-journalism-module-master/interactive-journalism-module-master/interactive-journalism-module-master/Data Files/world_populationWB.csv")
+names(world_populationWB) <- c("X1", "X2", "Country", "X4", "X5", "X6", "X7","X8", "X9", "X10", "X11", "X12", "X13", "X14","X15", "X16")
+world_populationWB.clean <- world_populationWB %>% 
+  select(-X1, -X2, -X4, -X5, -X6, -X7, -X8, -X9, -X10, -X11, -X12, -X13, -X14, -X16)
+names(world_populationWB.clean) <- c("region","population")
+
+#cleaning for matching with denmark immigration data#
+world_populationWB.clean_Final <- world_populationWB.clean[-c(4, 28, 29, 37, 40, 54, 69, 74, 75, 79, 94, 129, 141, 146, 150, 172, 181, 182, 183, 184, 194, 213, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269), ]
+
+#matching World Bank population data's country names with Denmark's immigration data country names in order to be able to merge data sets#
+# Code: data.frame[row_number, column_number] = new_value#
+
+world_populationWB.clean_Final[48, 1] = Ivory_Coast
+
+
+
+
