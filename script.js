12a58b2ae2437fdaf6253c4710533d27a9053ead
ChandralekhaMukerji <ChandralekhaMukerji@github.com>
2018-03-27 17:01:10 +0100

    adding data and styling

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     });
 })
 
-dropdownSelectionArray = ["array-1", "array-2", "array-3", "array-4"];
+dropdownSelectionArray = ["0", "9487", "246", "587", "801", "16", "300", "5", "2980", "878", "1", "15", "12422", "5715", "393", "25", "140", "2199", "55", "1217", "6309", "7", "124", "4", "374", "548", "22787", "97", "7845", "1", "30", "15280", "160", "501", "240", "1186", "11101", "20", "29", "30", "2010", "30729", "3452", "26", "805", "503", "3", "421", "2446", "651", "0", "612", "6322", "699", "98", "15", "191", "0", "3", "905", "2785", "96", "5", "4780", "5599", "4802", "10", "1", "603", "29", "11518", "26181", "4", "0", "0", "29", "952", "112", "375", "69576", "3299", "6439", "1016", "15", "1", "313", "130", "130", "60", "130", "251", "247", "11465", "33827", "26552", "0", "1578", "16996", "17787", "4621", "2617", "25369", "610", "204", "7758", "3123", "339", "3521", "7", "539", "504", "92", "76", "13336", "13795", "51", "163", "241", "44", "30117", "389", "1", "2567", "65", "227", "2313", "23", "68", "268", "1", "2", "35", "102", "3040", "79", "1098", "26", "164", "160", "4560", "598", "1006", "137", "5", "7109", "18914", "0", "2687", "276", "39", "1848", "11", "556", "10", "17", "51925", "11200", "135", "8", "16272", "146", "18", "187", "1207", "19503", "72724", "6416", "1", "344", "0", "43299", "10853", "447", "0", "6", "16", "5", "10", "4", "1", "1018", "201", "1610", "568", "25", "392", "0", "3051", "6240", "1286", "4", "14836", "1964", "24", "4596", "11", "0", "956", "25154", "0", "6474", "629", "956", "10", "86", "46414", "5855", "35641", "891", "40", "1697", "16409", "192", "21", "134", "0", "1063", "30946", "22", "3", "3024", "24362", "921", "40129", "188", "65655", "452", "5", "1", "928", "8089", "0", "9", "236", "9128", "1236", "1059", "710"];
 
 function dropdownSelection() {
     var e = document.getElementById("mySelect");
@@ -30,10 +30,8 @@ function dropdownSelection() {
     var y = document.getElementById("mySelect").options;
     var value = y[x].index;
     var text = y[x].text;
-    console.log("Value : " + value);
-    console.log("Text :" + text);
     if (value > 0) {
-        document.getElementById("mySelectResult").innerHTML = " Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoomnifoodncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus."
+        document.getElementById("mySelectResult").innerHTML = " In last 30 years, between 1987 and 2017, <b>" + dropdownSelectionArray[value] + "</b> immigrants have moved to <b>Denmark</b> from <b>" + text + "</b>."
     } else {
         document.getElementById("mySelectResult").innerHTML = "";
     }
@@ -69,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 'rgba(19,64,117,0.5)', 'rgba(19,64,117,0.6)', 'rgba(19,64,117,0.8)', 'rgba(19,64,117,1)'],
 
                 title: {
-                    text: 'Immigration from different countries (per 1000)'
+                    text: 'Immigration from different countries'
                 },
 
                 mapNavigation: {
@@ -78,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 legend: {
                     title: {
-                        text: 'Population per 1000',
+                        text: 'Immigrants per 1000',
                         style: {
                             color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
                         }
